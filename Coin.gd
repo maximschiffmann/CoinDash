c662c548e3d020150f3139d17c1c9654b64b50bc
Maxim Schiffmann <42582464+maximschiffmann@users.noreply.github.com>
2020-02-17 01:24:33 +0100

    Prototype

---
+++ Coin.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var screensize
+
+func _ready():
+	$Tween.interpolate_property($AnimatedSprite, 'scale', $AnimatedSprite.scale, $AnimatedSprite.scale * 3, 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$Tween.interpolate_property($AnimatedSprite, 'modulate', Color(1,1,1,1), Color(1,1,1,0), 0.3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+
+func pickup():
+	#queue_free()
+	monitoring = false
+	$Tween.start()
+
+func _on_Tween_tween_completed(object, key):
+	queue_free()
+
+func _on_Timer_timeout():
+	$AnimatedSprite.frame = 0
+	$AnimatedSprite.play()
+
+func _on_Coin_area_entered( area ):
+	if area.is_in_group("obstacles"):
+		position = Vector2(rand_range(30, screensize.x - 30), rand_range(100, screensize.y - 30))
